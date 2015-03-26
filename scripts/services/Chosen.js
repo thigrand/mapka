647629d2436b9b25a19536c84c839ae22fee5b7a
thigrand <thigrand@op.pl>
2015-03-26 17:03:52 +0100

    Delete trash

--- scripts/services/Chosen.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-function ChosenCity() {
-    function clicked(city) {
-        //console.log(city);
-    }
-    this.clicked = clicked;
-    return this;
-}
-angular.module('mapSVG').factory('ChosenCity',  ChosenCity);
